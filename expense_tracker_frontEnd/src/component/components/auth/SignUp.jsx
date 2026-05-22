d816b66737a8fef7cdd93b3c5a4e79a569af2307
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-22 14:42:16 +0530

    Sign Up

--- expense_tracker_frontEnd/src/component/components/auth/SignUp.jsx
+++ expense_tracker_frontEnd/src/component/components/auth/SignUp.jsx
@@ -1,8 +1,34 @@
+import axios from "axios";
 import CustomButton from "../../ui/CustomButton";
 import CustomInput from "../../ui/CustomInput";
 import CustomText from "../../ui/CustomText";
+import { useState } from "react";
 
 const SignUp=()=>{
+    const [singUpInput,setSignUpInput]=useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const signInputUpHandler=(e)=>{
+        const {name,value}=e.target;
+        setSignUpInput({...singUpInput,[name]:value});
+
+    }
+
+    const signUpHandler=async()=>{
+        const data={...singUpInput}
+        try {
+            const res=await axios.post("http://localhost:3000/users/signUp",data);
+            console.log(res);
+            
+            
+        } catch (error) {
+            console.log(error.message);
+            
+        }
+    }
     return(
         <>
          <div className="w-[50%] h-screen m-auto flex justify-center items-center">
@@ -10,18 +36,18 @@ const SignUp=()=>{
                 <CustomText className={"!text-[30px] text-center"} value={"Sign Up"}/>
                <div className="flex flex-col gap-2">
                 <CustomText value={"Name"}/>
-                <CustomInput className={"!w-[300px]"} />
+                <CustomInput name="name" value={singUpInput?.name} onchange={(e)=>{signInputUpHandler(e)}} className={"!w-[300px]"} />
                </div>
                <div className="flex flex-col gap-2">
                 <CustomText value={"Email"}/>
-                <CustomInput className={"!w-[300px]"} />
+                <CustomInput name="email" value={singUpInput?.email} onchange={(e)=>{signInputUpHandler(e)}} className={"!w-[300px]"} />
                </div>
                <div className="flex flex-col gap-2">
                 <CustomText value={"Password"}/>
-                <CustomInput className={"!w-[300px]"} />
+                <CustomInput name="password" value={singUpInput?.password} onchange={(e)=>{signInputUpHandler(e)}} className={"!w-[300px]"} />
                </div>
                <div className="flex justify-center pt-3">
-                <CustomButton value={"Sign Up"}/>
+                <CustomButton onclick={()=>{signUpHandler()}} value={"Sign Up"}/>
                </div>
          </div>
          </div>
